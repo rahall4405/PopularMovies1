16f400c301178887f904679f19eb7817cfa9d894
Robert Hall <rahall4405@gmail.com>
2016-04-11 10:17:20 -0500

    Updated gitignore and grades for android studio 2.0

--- app/build/generated/source/r/debug/nanodegree/rahall/popularmovies1/R.java
+++ app/build/generated/source/r/debug/nanodegree/rahall/popularmovies1/R.java
@@ -1664,7 +1664,11 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020038;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020039;
         public static final int abc_textfield_search_material=0x7f02003a;
-        public static final int notification_template_icon_bg=0x7f02003b;
+        public static final int device203118=0x7f02003b;
+        public static final int device203334=0x7f02003c;
+        public static final int device203506=0x7f02003d;
+        public static final int device203520=0x7f02003e;
+        public static final int notification_template_icon_bg=0x7f02003f;
     }
     public static final class id {
         public static final int action0=0x7f0c0070;
